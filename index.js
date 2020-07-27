0957f8cd3e334595e459ae4a5e4b7acf89b174ac
aniket <aniket.kh18@gmail.com>
2020-07-27 17:51:48 +0530

    fixed layout, added new images, added new typewriter effect

--- index.js
+++ index.js
@@ -3,6 +3,84 @@
 
 
 
+
+const TypeWriter = function(txtElement, words, wait = 800) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+}
+
+// Type Method
+TypeWriter.prototype.type = function() {
+  // Current index of word
+  const current = this.wordIndex % this.words.length;
+  // Get full text of current word
+  const fullTxt = this.words[current];
+
+  // Check if deleting
+  if(this.isDeleting) {
+    // Remove char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // Add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  // Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  // Initial Type Speed
+  let typeSpeed = 100;
+
+  if(this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  // If word is complete
+  if(!this.isDeleting && this.txt === fullTxt) {
+    // Make pause at end
+    typeSpeed = this.wait;
+    // Set delete to true
+    this.isDeleting = true;
+  } else if(this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    // Move to next word
+    this.wordIndex++;
+    // Pause before start typing
+    typeSpeed = 250;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
 
   setInterval(function(){
@@ -44,10 +122,10 @@ window.addEventListener("scroll",function(){
   var val = window.scrollY;
 console.log(val);
 $("#bg").css("top",val+0.5+"px");
-$("#moon").css("left",-val+0.3+"px");
-$("#text").css("letter-spacing",val/4+"px");
-$("#mountain").css("top",-val/7+0.10+"px");
-$("#road").css("top",val/7+0.10+"px");
+$("#moon").css("left",-val+0.5+"px");
+$("#text").css("letter-spacing",val/2+"px");
+$("#mountain").css("top",(-val/8)-50+"px");
+$("#road").css("top",val/8+0.10+"px");
 $("#text").css("top",val+"px");
 $("#second").css("clipPath","circle("+val+"px at center");
 
@@ -84,7 +162,7 @@ $(".link2, .link3").hover(function(){
 
 }
 
-else if(val>714 && val<2090){
+else if(val>714 && val<2010){
 
 $(".link2").css({"color":"#00f3ff",
 "textShadow":"0 0 15px #00f3ff"
@@ -112,7 +190,7 @@ $(".link1, .link3").hover(function(){
 });
 
 }
-else if(val>2090){
+else if(val>2010){
 
   $(".link3").css({"color":"#00f3ff",
   "textShadow":"0 0 15px #00f3ff"
